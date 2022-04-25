d532c34a51f7c82282de8dc3a540fbfe0debee17
Jake-Mcclure <62794175+Jake-Mcclure@users.noreply.github.com>
2022-04-25 11:40:51 +0100

    many differences

--- Assets/Scripts/Game_Controller.cs
+++ Assets/Scripts/Game_Controller.cs
@@ -5,30 +5,12 @@ using UnityEngine.UI;
 
 public class Game_Controller : MonoBehaviour
 {
-    
-    public Text BestTime;
-    public Text CurrentTime;
 
-    private float m_time = 0.0f;
-    private float m_bestTime = 0.0f;
-    private string m_txtbesttime;
-    private string m_txtcurrenttime;
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
+    private float[][] m_InitialData;
+    private string[] m_InitialString;
 
-    // Update is called once per frame
-    void Update()
-    {
-        m_time += Time.deltaTime;
-        CurrentTime.text = m_time.ToString();
-    }
+    public List<string> LoadedInformation;
 
-    public void LapComplete()
-    {
-
-    }
-
+    private float m_MutationRate;
+    private float m_MutationProbability;
 }
--- Assets/Scripts/TheNetwork.cs
+++ Assets/Scripts/TheNetwork.cs
@@ -4,10 +4,123 @@ using UnityEngine;
 
 public class TheNetwork : MonoBehaviour
 {
-    
+
+    float[] m_Hiddenlayer;
+    float[][] m_HiddenLWeights;
+    float[] m_HiddenLBias;
+
+    int m_Inputs;
+
+    float[] m_OutputWeights;
+    float m_OutputBias;
+
+    float m_Ability;
 
     public TheNetwork(int inputs, int Hiddennodes)
     {
+        m_Hiddenlayer = new float[Hiddennodes];
+        m_Inputs = inputs;
 
     }
+
+    public void SetAbility(float ability)
+    {
+        m_Ability = ability;
+    }
+    //this makes all weights equal to 0
+    public void InitialiseWeights(int inputs)
+    {
+        m_Inputs = inputs;
+
+        m_HiddenLWeights = new float[m_Hiddenlayer.Length][];
+        m_HiddenLBias = new float[m_HiddenLWeights.Length];
+
+        m_OutputWeights = new float[m_Hiddenlayer.Length];
+
+        for (int i = 0; i < m_HiddenLWeights.GetLength(0); i++)
+        {
+            m_HiddenLWeights[i] = new float[inputs];
+            m_HiddenLBias[i] = 0;
+            m_OutputWeights[i] = 0;
+            for (int x = 0; x < m_HiddenLWeights.Length; x++)
+            {
+                m_HiddenLWeights[i][x] = 0;
+            }
+
+        }
+    }
+
+    public void InitialiseWeights(float[] Weights)
+    {
+        for (int i = 0; i < m_HiddenLWeights.Length; i++)
+        {
+            for (int x = 0; x < m_HiddenLWeights[i].Length; x++)
+            {
+                m_HiddenLWeights[i][x] = Weights[x + (6*i)];
+            }
+        }
+    }
+
+    public float CalculateOutput(float[] inputs)
+    {
+        for (int i = 0; i < m_Hiddenlayer.Length; i++)
+        {
+            m_Hiddenlayer[i] = Mathf.Max(0, Sum(m_OutputWeights, m_Hiddenlayer));
+        }
+        float output = Limit(Sum(m_OutputWeights, m_Hiddenlayer) + m_OutputBias);
+
+        return output;
+    }
+
+    public float Sum(float[] a, float[] b)
+    {
+        float temp = 0;
+        for (int i = 0; i < a.Length; i++)
+        {
+            temp += a[i] * b[i];
+        }
+        return temp;
+    }
+
+    float Limit(float input)
+    {
+        return input / (1+Mathf.Abs(input));
+    }
+
+    //this basically gives the information/details about this brain
+    public string ReadBrain()
+    {
+        string information = "";
+
+        for (int i = 0; i < m_HiddenLWeights.Length; i++)
+        {
+            for (int x = 0; x < m_HiddenLWeights[i].Length; x++)
+            {
+                information += m_HiddenLWeights[i][x] + ",";
+            }
+            information += m_HiddenLBias[i] + ",";
+        }
+        for (int i = 0; i < m_OutputWeights.Length; i++)
+        {
+            information += m_OutputWeights[i] + ",";
+        }
+        information += m_OutputBias;
+
+        return information;
+    }
+
+    //this transfers the information from string format to an array of float values
+    public float[] GetBrain()
+    {
+        string[] information = ReadBrain().Split(',');
+        float[] temp = new float[information.Length];
+
+        for (int i = 0; i < information.Length; i++)
+        {
+            temp[i] = float.Parse(information[i]);
+        }
+
+        return temp;
+    }
+
 }
